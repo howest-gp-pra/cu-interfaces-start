f0396f2033ef19562bc5bda33a3189bc8dcda4f6
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-09-18 13:02:55 +0200

    Add project files.

---
+++ Pra.Interfaces.WPF/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pra.Interfaces.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnTVLivingRoomPower_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSmartLampHallwayPower_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRadioKitchenPower_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnTVLivingRoomVolumeDown_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnTVLivingRoomVolumeUp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRadioKitchenVolumeDown_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRadioKitchenVolumeUp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCheckConnections_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCheckInterfaceImplementation_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
